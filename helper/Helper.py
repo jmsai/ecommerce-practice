8aa9bc12d473c4774a2925d88ab9b3e87777a5bd
Jemimah Sai <jmsai@trends.com.ph>
2019-05-21 17:48:14 +0800

    Add helper methods for app

---
+++ helper/Helper.py
@@ -0,0 +1,14 @@
+import json
+import uuid
+
+def convert_to_json(obj):
+    return json.dumps(obj, indent=4, sort_keys=False)
+
+def generate_id():
+    return str(uuid.uuid4())
+
+def generate_sku():
+    return str(uuid.uuid4().fields[-1])[:8]
+
+def generate_tracking_number():
+    return str(uuid.uuid4().fields[-1])[:12]
