7d6e9abfa8fed9eef5f1445e5658bc0177fc185b
Jemimah Sai <jmsai@trends.com.ph>
2019-05-24 17:01:09 +0800

    Modify feature to generate an id

--- helper/Helper.py
+++ helper/Helper.py
@@ -5,9 +5,6 @@ def convert_to_json(obj):
     return json.dumps(obj, indent=4, sort_keys=False)
 
 def generate_id():
-    return str(uuid.uuid4())
-
-def generate_sku():
     return str(uuid.uuid4().fields[-1])[:8]
 
 def generate_tracking_number():
