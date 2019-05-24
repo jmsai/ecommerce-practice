ee8159414647ca21c4163b7247228f65fae57947
Jemimah Sai <jmsai@trends.com.ph>
2019-05-24 10:34:55 +0800

    Modify model Order

--- model/Order.py
+++ model/Order.py
@@ -6,35 +6,22 @@ sys.path.append(path.join(path.dirname(__file__), '..'))
 from helper.Helper import convert_to_json, generate_sku, generate_tracking_number
 
 class Order:
-    def __init__(self, transaction_date, delivery_time, customer_id, shipping_fee, shipping_address, billing_address, payment_method, grand_total, tax_amount, discount_total, items):
-        self.order_number = generate_sku()
+    def __init__(self, delivery_date, delivery_status, shipping_fee, 
+                 shipping_address, billing_address, transaction_date, 
+                 payment_status, payment_method, payment_total, tax_amount, 
+                 discount_total, items, customer_id):
+        self.order_id = generate_sku()
         self.tracking_id = generate_tracking_number()
-        self.transaction_date = transaction_date
-        self.delivery_time = delivery_time
-        self.customer_id = customer_id
+        self.delivery_date = delivery_date
+        self.delivery_status = delivery_status
         self.shipping_address = shipping_address
         self.billing_address = billing_address
+        self.transaction_date = transaction_date
+        self.payment_status = payment_status
         self.payment_method = payment_method
         self.tax_amount = tax_amount
         self.shipping_fee = shipping_fee
         self.discount_total = discount_total
-        self.grand_total = grand_total
+        self.payment_total = payment_total
         self.items = items
-
-    def display_json(self):
-        order = {
-            "order_numer": self.order_number,
-            "tracking_id": self.tracking_id,
-            "transaction_date": self.transaction_date,
-            "delivery_time": self.delivery_time,
-            "customer_id": self.customer_id,
-            "shipping_address": self.shipping_address,
-            "billing_address": self.billing_address,
-            "payment_method": self.payment_method,
-            "tax_amount": self.tax_amount,
-            "shipping_fee": self.shipping_fee,
-            "discount_total": self.discount_total,
-            "grand_total": self.grand_total,
-            "items": self.items            
-        }
-        return convert_to_json(order)
+        self.customer_id = customer_id
