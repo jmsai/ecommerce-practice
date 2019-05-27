cd363faba32ea912b13ce3ba26dc492ff449e53c
Jemimah Sai <jmsai@trends.com.ph>
2019-05-27 11:54:21 +0800

    Delete unnecessary files and setup requirements.txt

--- instance/flask.py
+++
@@ -1,5 +0,0 @@
-import os
-
-BASEDIR = os.path.abspath(os.path.dirname(__file__))
-DEBUG = True
-BCRYPT_LOG_ROUNDS = 15
--- view/Cart.py
+++
@@ -1,7 +0,0 @@
-from os import path
-import sys
-sys.path.append(path.join(path.dirname(__file__), '..'))
-from helper.Helper import convert_to_json
-
-def cart_response(cart):
-    convert_to_json(cart)
