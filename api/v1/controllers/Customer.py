ef6d8ef4c54115ba3169ec9f04bb8569fc739d60
Jemimah Sai <jmsai@trends.com.ph>
2019-06-13 17:58:09 +0800

    modify Customer controller to implement error handling

--- api/v1/controllers/Customer.py
+++ api/v1/controllers/Customer.py
@@ -1,5 +1,9 @@
 from api.v1.models.Customer import Customer
 from api.v1.views.CustomerView import CustomerView
+from api.v1.models.Error import NotFoundError
+from api.v1.models.Error import BadRequestError
+from api.v1.models.Error import UnprocessableEntityError
+from api.v1.models.Error import ResourceAlreadyExistError
 from api.v1.views.ErrorView import ErrorView
 
 from flask import request
@@ -7,7 +11,7 @@ from flask_restful import Resource
 
 model = Customer()
 view = CustomerView()
-Error = ErrorView()
+ErrorView = ErrorView()
 
 
 class CustomerController(Resource):
@@ -16,9 +20,13 @@ class CustomerController(Resource):
             customer = model.find_by(_id)
             customer["middle_initial"] = model.get_middle_initial(customer)
             customer["full_name"] = model.get_full_name(customer)
-            return view.display(customer), 200
+
         except:
-            return Error.customer_not_found(), 404
+            error = NotFoundError("Customer")
+            return ErrorView.display(error), 404
+
+        else:
+            return view.display(customer), 200
 
 
 class SignupController(Resource):
@@ -28,8 +36,14 @@ class SignupController(Resource):
             customer = model.find_by_email(data["email"])
 
             if customer is not None:
-                return Error.user_already_exist(), 400
+                error = ResourceAlreadyExistError("User")
+                return ErrorView.display(error), 409
 
+        except:
+            error = UnprocessableEntityError()
+            return ErrorView.display(error), 422
+
+        else:
             new_customer = Customer(
                                     data["email"],
                                     data["password"],
@@ -42,28 +56,30 @@ class SignupController(Resource):
 
             return view.display_credentials(new_customer), 201
 
-        except:
-            Error.failed_to_perform_action(), 422
-
 
 class LoginController(Resource):
     def post(self):
+        data = request.get_json()
         try:
-            data = request.get_json()
             customer = model.find_by_email(data.get('email'))
 
             if customer is None:
-                return Error.user_not_found(), 404
+                error = NotFoundError("Customer")
+                return ErrorView.display(error), 404
 
+        except:
+            error = UnprocessableEntityError()
+            return ErrorView.display(error), 422
+
+        else:
             input_password = data["password"]
             password = customer['password']
 
             valid_password = model.is_password_valid(input_password, password)
 
             if not valid_password:
-                return Error.invalid_login(), 400
+                error = BadRequestError("Login failed",
+                                        "Password does not match")
+                return ErrorView.display(error), 400
 
             return view.display_credentials(customer), 200
-
-        except:
-            return Error.failed_to_perform_action(), 422
