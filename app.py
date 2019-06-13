e6ef63ceabdd03a5e430a8e36a330bdf3d948dfb
Jemimah Sai <jmsai@trends.com.ph>
2019-06-13 17:57:52 +0800

    register error handlers version 1 to app.py

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from api.v1.routes import api_v1
 from api.v2.routes import api_v2
+from api.v1.error import handle_page_not_found
+from api.v1.error import handle_unauthorized_error
+from api.v1.error import handle_access_denied_error
+from api.v1.error import handle_internal_server_error
 
 from flask import Flask, Blueprint
 from flask_restful import Api
@@ -10,10 +14,10 @@ api = Api(app)
 app.register_blueprint(api_v1, url_prefix='/api/v1')
 app.register_blueprint(api_v2, url_prefix='/api/v2')
 
-# Error Page Route
-# @app.errorhandler(404)
-# def page_not_found(e):
-#     return "Error 404: Page not found!"
+app.register_error_handler(404, handle_page_not_found)
+app.register_error_handler(401, handle_unauthorized_error)
+app.register_error_handler(403, handle_access_denied_error)
+app.register_error_handler(500, handle_internal_server_error)
 
 if __name__ == '__main__':
     app.run(port=3000, debug=True)
